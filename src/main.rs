ba0f1c31760971384b89b3f7ebfb08dd06f67050
Assxios <droge@student.42.fr>
2024-01-10 20:08:21 +0100

    Update: it works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -9,6 +9,6 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
