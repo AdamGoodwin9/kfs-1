b548dfe4c174bb8046c1ca35c447fda6adcca3fa
Assxios <droge@student.42.fr>
2024-03-28 03:18:41 +0100

    Update: vga functionnal with helpers functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,20 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
+mod helpers;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // don't mangle the name of this function (changing the name)
-pub extern "C" fn kernel_main() -> ! {
+pub extern "C" fn kernel_main()
+{
     // this function is the entry point
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
     loop {}
 }
